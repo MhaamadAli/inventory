201403864177fb13a255f89b9b6b94f46bc355a7
MhaamadAli <m.alialaawar@gmail.com>
2024-07-31 17:33:45 +0300

    initilized the project with nextjs and firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGyDtpVdp2aiWrHZBN-tnBqSTmMbResnI",
+  authDomain: "inventory-management-55e0a.firebaseapp.com",
+  projectId: "inventory-management-55e0a",
+  storageBucket: "inventory-management-55e0a.appspot.com",
+  messagingSenderId: "15384666955",
+  appId: "1:15384666955:web:a47837202f2b3d41ccbece",
+  measurementId: "G-NJSS57ED4G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
