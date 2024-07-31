e371713974974649eff71ffe4255007578053e45
MhaamadAli <m.alialaawar@gmail.com>
2024-07-31 23:44:11 +0300

    added basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "../../firebase";
 import {
@@ -14,7 +13,14 @@ import {
   doc,
   getDoc,
 } from "firebase/firestore";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Modal,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 interface InventoryItem {
   name: string;
@@ -24,7 +30,7 @@ interface InventoryItem {
 export default function Home() {
   const [inventory, setInventory] = useState<InventoryItem[]>([]);
   const [open, setOpen] = useState<boolean>(false);
-  const [itemName, setItemName] = useState<string[]>([]);
+  const [itemName, setItemName] = useState<string>("");
 
   const updateInventory = async (): Promise<void> => {
     try {
@@ -85,18 +91,94 @@ export default function Home() {
     updateInventory();
   }, []);
 
-  const handlOpen = () => setOpen(true)
-  const handlCloae = () => setOpen(false);
-  
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setItemName(""); // Reset item name when closing the modal
+  };
+
   return (
-    <Box>
-      <Typography variant="h1">Inventory Management</Typography>
-      {inventory.map((item, index) => (
-        <Box key={index}>
-          <Typography variant="body1">{item.name}</Typography>
-          <Typography variant="body2">{item.quantity}</Typography>
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{ padding: 2, backgroundColor: "white", borderRadius: 1 }}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName);
+                handleClose();
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
         </Box>
-      ))}
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>
+        Add New Item
+      </Button>
+      <Box border="1px solid #333" overflow="auto">
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor="#ADD8E6"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h2" color="#333" textAlign="center">
+            Inventory Items
+          </Typography>
+        </Box>
+        <Stack width="800px" spacing={2} padding={2}>
+          {inventory.map(({ name, quantity }) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              bgcolor="#f0f0f0"
+              paddingX={5}
+            >
+              <Typography variant="h5" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant="h5" color="#333" textAlign="center">
+                Quantity: {quantity}
+              </Typography>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                Remove
+              </Button>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
     </Box>
   );
 }
